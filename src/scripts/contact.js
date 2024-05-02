039c24eda776c36d8e842729b1b25b675bea8961
Ildi Dvorani <dvoranii@ildidev.com>
2024-05-02 18:31:12 -0400

    updated contact form html, and added the grecaptcha logic as well as configured it to make fetch request to my backend

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,3 +1,51 @@
 import "../styles/contact.css";
 
-console.log("This is contact");
+document.addEventListener("DOMContentLoaded", () => {
+  setupRecaptcha();
+  setupFormSubmission();
+});
+
+function setupFormSubmission() {
+  const form = document.querySelector(".contact-form");
+  if (!form) {
+    console.error("Form not found on the page");
+    return;
+  }
+  form.addEventListener("submit", handleSubmit);
+}
+
+function setupRecaptcha() {
+  if (typeof grecaptcha === "undefined") {
+    console.error("reCAPTCHA library not loaded!");
+    return;
+  }
+  grecaptcha.ready(() => {
+    grecaptcha
+      .execute("6LddpsMpAAAAAD-7Uj4O_xlo84BMGwjJp_rQBkX1", { action: "submit" })
+      .then((token) => {
+        let recaptchaResponse = document.getElementById("recaptchaResponse");
+        if (!recaptchaResponse) {
+          console.error("Recaptcha response element not found!");
+          return;
+        }
+        recaptchaResponse.value = token;
+      });
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  fetch("http://localhost:4444/submit-form", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
