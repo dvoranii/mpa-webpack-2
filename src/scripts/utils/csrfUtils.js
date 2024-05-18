1ae9ccf56afef47aca7cabe68bb9c2a991fbb617
Ildi Dvorani <dvoranii@ildidev.com>
2024-05-18 12:08:49 -0400

    removed comments

--- src/scripts/utils/csrfUtils.js
+++ src/scripts/utils/csrfUtils.js
@@ -5,10 +5,10 @@ let csrfToken = null;
 export async function fetchCsrfToken() {
   try {
     const response = await fetch("http://localhost:4444/csrf-token", {
-      credentials: "include", // Ensure cookies are included in requests
+      credentials: "include",
     });
-    console.log("Response status:", response.status); // Log the status code
-    console.log("Response headers:", response.headers.get("content-type")); // Log content type
+    console.log("Response status:", response.status);
+    console.log("Response headers:", response.headers.get("content-type"));
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -20,10 +20,10 @@ export async function fetchCsrfToken() {
 
     const data = await response.json();
     console.log("CSRF token received:", data.csrfToken);
-    csrfToken = data.csrfToken; // Store the token for later use
+    csrfToken = data.csrfToken;
   } catch (error) {
     console.error("Error fetching CSRF token:", error);
-    throw error; // Re-throw the error so it can be caught in the calling function
+    throw error;
   }
 }
 
