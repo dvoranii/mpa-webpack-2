6fca88ec6c5852fe9c13de541268fb126a00044b
Ildi Dvorani <dvoranii@ildidev.com>
2024-06-18 17:24:00 -0400

    made footer mobile responsive

--- src/scripts/utils/basicScroll.js
+++ src/scripts/utils/basicScroll.js
@@ -12,52 +12,65 @@ function createBasicScroll(elem, from, to, props) {
   return instance;
 }
 
+function initializeBasicScrollInstances() {
+  const scrollInstances = [
+    createBasicScroll(".scroll-img--1", "top-bottom", "bottom-top", {
+      "--translateX1": {
+        from: "20vw",
+        to: "-40vw",
+      },
+    }),
+    createBasicScroll(".scroll-img--2", "top-bottom", "bottom-top", {
+      "--translateX2": {
+        from: "40vw",
+        to: "0vw",
+      },
+    }),
+    createBasicScroll(".scroll-img--3", "top-bottom", "bottom-top", {
+      "--translateX3": {
+        from: "20vw",
+        to: "-40vw",
+      },
+    }),
+    createBasicScroll(".scroll-img--4", "top-bottom", "bottom-top", {
+      "--translateX4": {
+        from: "40vw",
+        to: "0vw",
+      },
+    }),
+    createBasicScroll(
+      ".home-section__logo-wrapper",
+      "top-bottom",
+      "top-center",
+      {
+        "--logoBgOpacity": {
+          from: "0",
+          to: "0.7",
+        },
+      }
+    ),
+    createBasicScroll(".bg", "viewport-top", "top-top", {
+      "--bg-opacity": {
+        from: "1",
+        to: "0.01",
+      },
+    }),
+    createBasicScroll(".card", "top-bottom", "bottom-center", {
+      "--shadowOpacity": {
+        from: "0.0",
+        to: "0.6",
+      },
+    }),
+  ];
+
+  return scrollInstances;
+}
+
 export function initializeBasicScroll() {
-  createBasicScroll(".scroll-img--1", "top-bottom", "bottom-top", {
-    "--translateX1": {
-      from: "20vw",
-      to: "-40vw",
-    },
-  });
+  let scrollInstances = initializeBasicScrollInstances();
 
-  createBasicScroll(".scroll-img--2", "top-bottom", "bottom-top", {
-    "--translateX2": {
-      from: "40vw",
-      to: "0vw",
-    },
-  });
-
-  createBasicScroll(".scroll-img--3", "top-bottom", "bottom-top", {
-    "--translateX3": {
-      from: "20vw",
-      to: "-40vw",
-    },
-  });
-
-  createBasicScroll(".scroll-img--4", "top-bottom", "bottom-top", {
-    "--translateX4": {
-      from: "40vw",
-      to: "0vw",
-    },
-  });
-  createBasicScroll(".home-section__logo-wrapper", "top-bottom", "top-center", {
-    "--logoBgOpacity": {
-      from: "0",
-      to: "0.7",
-    },
-  });
-
-  createBasicScroll(".bg", "viewport-top", "top-top", {
-    "--bg-opacity": {
-      from: "1",
-      to: "0.01",
-    },
-  });
-
-  createBasicScroll(".card", "top-bottom", "bottom-center", {
-    "--shadowOpacity": {
-      from: "0.0",
-      to: "0.6",
-    },
+  window.addEventListener("resize", () => {
+    scrollInstances.forEach((instance) => instance.destroy());
+    scrollInstances = initializeBasicScrollInstances();
   });
 }
