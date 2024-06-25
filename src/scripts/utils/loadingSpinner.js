18cc3f736f52468b13e0240198d3223173eba6e2
Ildi Dvorani <dvoranii@ildidev.com>
2024-06-25 11:22:49 -0400

    removed console logs and comment

--- src/scripts/utils/loadingSpinner.js
+++ src/scripts/utils/loadingSpinner.js
@@ -1,14 +1,9 @@
-// loadingSpinner.js
 export function showLoader(loader, triggerBtn) {
-  console.log("Showing loader:", loader);
   loader.classList.remove("loader-hidden");
-  console.log("Loader classes after showing:", loader.classList);
   triggerBtn.style.visibility = "hidden";
 }
 
 export function hideLoader(loader, triggerBtn) {
   loader.classList.add("loader-hidden");
-  console.log("Hiding loader:", loader);
-  console.log("Loader classes after hiding:", loader.classList);
   triggerBtn.style.visibility = "visible";
 }
